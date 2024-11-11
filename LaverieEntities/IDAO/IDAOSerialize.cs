ce35040b2022f5472e448cb27c783740147092d0
islem.mellehi <islemmellehi222@gmail.com>
2024-11-11 21:38:52 +0100

    added serialization IDAO

---
+++ LaverieEntities/IDAO/IDAOSerialize.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Laverie.DTO;
+namespace LaverieEntities.IDAO
+{
+    public interface IDAOSerialize
+    {
+        public LaverieData GetAllData();
+
+    }
+}
--- LaverieEntities/obj/Debug/net8.0/LaverieEntities.AssemblyInfo.cs
+++ LaverieEntities/obj/Debug/net8.0/LaverieEntities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaverieEntities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cd8baeac19ae479218ec750f260079218e59020")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaverieEntities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaverieEntities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
